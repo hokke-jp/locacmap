bb5c6358f7dc399bf647c66e9cc7e8640a7b4458
burihokke <burihokke@gmail.com>
2020-12-25 21:20:10 +0900

    タイトルヘルパー作成

--- test/controllers/peges_controller_test.rb
+++ test/controllers/peges_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class PegesControllerTest < ActionDispatch::IntegrationTest
+  test "shoud get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Localmap"
+  end
+
   test "should get home" do
     get peges_home_url
     assert_response :success
+    assert_select "title", "Home | Localmap"
   end
 end
