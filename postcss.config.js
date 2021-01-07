d6520285c3a02e9855403ef7d5c2fad4fc24449c
burihokke <burihokke@gmail.com>
2021-01-07 20:36:15 +0900

    Tailwind本番環境設定

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const environment = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
@@ -12,3 +12,19 @@ module.exports = {
     })
   ]
 }
+
+// 使っているクラスだけを抽出する設定
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/**/*.js.erb',
+        './app/helpers/**/*.rb',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment // エクスポート
