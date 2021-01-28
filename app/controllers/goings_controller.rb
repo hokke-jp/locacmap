ab64f4ac3729781b557c2a06fc206bcec5caad2e
hokke-jp <burihokke@gmail.com>
2021-01-28 13:56:54 +0900

    #10 非同期行ってみたいボタン実装

--- app/controllers/goings_controller.rb
+++ app/controllers/goings_controller.rb
@@ -1,13 +1,26 @@
 class GoingsController < ApplicationController
   before_action :logged_in_user
+  before_action :micropost_params
 
   def create
     Going.create(user_id: current_user.id, micropost_id: params[:id])
-    redirect_to microposts_url
+    respond_to do |format|
+      format.html { redirect_to microposts }
+      format.js
+    end
   end
 
   def destroy
     Going.find_by(user_id: current_user.id, micropost_id: params[:id]).destroy
-    redirect_to microposts_url
+    respond_to do |format|
+      format.html { redirect_to microposts }
+      format.js
+    end
+  end
+
+  private
+
+  def micropost_params
+    @micropost = Micropost.find(params[:id])
   end
 end
