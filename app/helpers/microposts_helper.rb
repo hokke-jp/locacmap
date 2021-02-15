af030cd4c7c033127985b94678f558cc5a874178
hokke-jp <burihokke@gmail.com>
2021-02-15 19:09:09 +0900

    #61 マップ情報ウィンドに表示される投稿のUI変更

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -3,14 +3,14 @@ module MicropostsHelper
   def image_for(micropost)
     # size = options[:size]
     if micropost.image.attached?
-      image_tag(micropost.image)
+      image_tag(micropost.image, width: '100%')
       # image_tag(micropost.image.variant(gravity: :center,
       #                                   resize:"160x120^",
       #                                   crop:"160x120+0+0"),
       #           id: "micropost#{micropost.id}-image", alt: 'micropost-image',
       #           class: '')
     else
-      image_tag('no_image.png')
+      image_tag('no_image.png', width: '100%')
       # image_tag('no_image.png', :size => '160x120')
     end
   end
